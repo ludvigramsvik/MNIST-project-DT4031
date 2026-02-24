a095011217f15ec678275ee75c12e832ef08083c
Ludvig Rämsvik <0404ludvig.ramsvik@gmail.com>
2026-02-24 14:27:10 +0100

    fs

---
+++ load_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+import struct
+
+def load_images(filepath):
+    with open(filepath, 'rb') as f:
+        magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
+        images = np.frombuffer(f.read(), dtype=np.uint8)
+        images = images.reshape(num, rows, cols)
+    return images
+
+def load_labels(filepath):
+    with open(filepath, 'rb') as f:
+        magic, num = struct.unpack(">II", f.read(8))
+        labels = np.frombuffer(f.read(), dtype=np.uint8)
+    return labels
